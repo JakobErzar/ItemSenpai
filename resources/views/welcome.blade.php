0b280419cec96990f18e6d41e4978fee8427b908
Jakob Erzar <jakob.erzar@gmail.com>
2015-08-13 23:10:22 +0200

    its black background now!!!!!

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 		<title>Laravel</title>
 		
 		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
 
 		<style>
 			body {
