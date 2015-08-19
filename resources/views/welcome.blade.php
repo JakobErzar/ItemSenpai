a779937c42e451528478fe8192ec34a3d7828670
Jakob Erzar <jakob.erzar@gmail.com>
2015-08-19 19:56:59 +0200

    Some of the basic models removed, first serious (demo kind of) commit. Features stuff from web_pure.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,20 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
-    	<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
-
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
-
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
-
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
-
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Item Senpai</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+@extends('app')
+@section('title', 'Item Senpai')
+@section('content')
+    <div class="text-center">
+        <h1>Item Senpai</h1>
+        <div class="lead">
+            Let the senpai show you the ways of the item sets.
+        </div>
+    </div>
+    <div class="row text-center choice">
+        <div class="col-md-6 pull-left fun">
+            <h2>FUN</h2>
+            <img src="http://www.noobabble.com/wp-content/uploads/2015/02/Bard-Splash-Art1.jpg" width="300px" height="150px" alt="">
+        </div>
+        <div class="col-md-6 pull-left work">
+            <h2>WORK</h2>
+            <img src="http://i.ytimg.com/vi/yUK5WeifdTw/maxresdefault.jpg" width="300px" height="150px" alt="">
+        </div>
+    </div>
+@stop
