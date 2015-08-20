ec6c0e0b3259152efe0f6819299fb228121a3c2a
Jakob Erzar <jakob.erzar@gmail.com>
2015-08-20 18:15:41 +0200

    Changed the cdn version, pictures should be working now

--- app/Http/Controllers/FillDataController.php
+++ app/Http/Controllers/FillDataController.php
@@ -9,8 +9,10 @@ use App\Champion;
 use App\Spell;
 
 class FillDataController extends Controller {
-
+    
 	public function champions() {
+        $cdn_ver = '5.15.1';
+        
         $json = file_get_contents('https://global.api.pvp.net/api/lol/static-data/euw/v1.2/champion?champData=all&api_key=2767f871-228a-412a-9063-d754163404f4');
         $obj = json_decode($json);
         
@@ -18,7 +20,7 @@ class FillDataController extends Controller {
         foreach($obj->data as $champ){
          
             $champion = Champion::firstOrNew(['riot_id' => $champ->id, 'key' => $champ->key, 'name' => $champ->name]);
-            $champion->icon = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/champion/'.$champ->key.'.png';
+            $champion->icon = 'http://ddragon.leagueoflegends.com/cdn/'.$cdn_ver.'/img/champion/'.$champ->image->full;
             $champion->splash = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/'.$champ->key.'_0.jpg';
             $champion->loading = 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/'.$champ->key.'_0.jpg';
             if(count($champ->tags) > 0) $champion->role1 = $champ->tags[0];
@@ -31,7 +33,7 @@ class FillDataController extends Controller {
                 $new_spell = Spell::firstOrNew(['name' => $spell->name, 'key' => $spell->key, 'champion_id' => $champ->id]);
                 $new_spell->max_rank = $spell->maxrank;
                 $new_spell->symbol = $symbols[$i];
-                $new_spell->icon = 'http://ddragon.leagueoflegends.com/cdn/5.2.1/img/spell/'.$spell->key.'.png';
+                $new_spell->icon = 'http://ddragon.leagueoflegends.com/cdn/'.$cdn_ver.'/img/spell/'.$spell->image->full;
                 $new_spell->save();
                 array_push($spells, $new_spell);
             }
